bc170f58de82000ed6460f111686a850a1890c07
Costin Manolache <costin@google.com>
2011-02-17 15:06:17 -0800

    Switch platform to 8, to allow unbundling/backporting of GoogleAuthenticator
    Change-Id: Ic7d89d382cee09c4cf07bf630ff8073a7987b65a

--- core/src/main/java/Android.mk
+++ core/src/main/java/Android.mk
@@ -8,7 +8,7 @@ LOCAL_MODULE := oauth
 LOCAL_SRC_FILES := \
    $(call all-java-files-under, net)
 
-LOCAL_SDK_VERSION := current
+LOCAL_SDK_VERSION := 8
 
 # Build the actual static library
 include $(BUILD_STATIC_JAVA_LIBRARY)
